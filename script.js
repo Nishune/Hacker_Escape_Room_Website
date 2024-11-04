60989b7b2a3f058c69d99b15f487a888a991f716
Rikard Engström <rikardengstrom89@gmail.com>
2024-11-04 19:25:12 +0100

    Added background blur behind the overlay by adding a div in html <header>, added new scss for the blur. Added new javascript to not be able to scroll when the overlay is active, added JS to activate the blur effect when clicking on the button, also removing it while exiting the overlay.

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 const menuButton = document.querySelector(".header__menu-button");
 const closeButton = document.querySelector(".header__navigation-close");
 const navigationMenu = document.querySelector(".header__navigation");
+const overlayBlur = document.querySelector(".overlay__blur");
 
 menuButton.addEventListener("click", () => {
     navigationMenu.classList.add("overlay");
+    overlayBlur.classList.add("active");
+    document.body.style.overflow = "hidden";
 });
 
 closeButton.addEventListener("click", () => {
     navigationMenu.classList.remove("overlay");
+    overlayBlur.classList.remove("active");
+    document.body.style.overflow = "";
 });
+
+overlayBlur.addEventListener("click", () => {
+    navigationMenu.classList.remove("overlay");
+    overlayBlur.classList.remove("active");
+    document.activeElement.style.overflow = "";
+});
+
