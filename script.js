2f3d7a3e8178d16449799ad1d8f5437c132fe1b9
Rikard Engström <rikardengstrom89@gmail.com>
2024-11-07 13:57:49 +0100

    Tried another approach in JS with function, mostly for training and fun. The other version worked aswell but this one felt cleaner.

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
+//variables
+
 const menuButton = document.querySelector(".header__menu-button");
 const closeButton = document.querySelector(".header__navigation-close");
 const navigationMenu = document.querySelector(".header__navigation");
 const overlayBlur = document.querySelector(".overlay__blur");
 
-menuButton.addEventListener("click", () => {
+//event-listeners
+menuButton.addEventListener("click", showMenu);
+closeButton.addEventListener("click", hideMenu);
+overlayBlur.addEventListener("click", hideMenu);
+
+function showMenu() {
     navigationMenu.classList.add("overlay");
     overlayBlur.classList.add("active");
     document.body.style.overflow = "hidden";
-});
+};
 
-closeButton.addEventListener("click", () => {
+function hideMenu() {
     navigationMenu.classList.remove("overlay");
     overlayBlur.classList.remove("active");
     document.body.style.overflow = "";
-});
-
-overlayBlur.addEventListener("click", () => {
-    navigationMenu.classList.remove("overlay");
-    overlayBlur.classList.remove("active");
-    document.activeElement.style.overflow = "";
-});
+};
 
